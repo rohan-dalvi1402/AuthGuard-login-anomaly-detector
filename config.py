4a0441649bd759f38ed70b634aae8a8db8ac4190
Rohan Jayawant Dalvi <87616784+rohan-dalvi1402@users.noreply.github.com>
2026-06-23 00:25:02 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+FAILURE_THRESHOLD = int(os.getenv("FAILURE_THRESHOLD", 5))
+TIME_WINDOW_SECONDS = int(os.getenv("TIME_WINDOW_SECONDS", 60))
+BLOCK_DURATION_SECONDS = int(os.getenv("BLOCK_DURATION_SECONDS", 300))
+CREDENTIAL_STUFFING_THRESHOLD = int(os.getenv("CREDENTIAL_STUFFING_THRESHOLD", 5))
+LOG_FILE = os.getenv("LOG_FILE", "logs/authguard.log")
